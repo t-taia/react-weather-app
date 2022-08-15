6948559b454f3f6a841ec8b0a29df99e30446891
t-taia <95856208+t-taia@users.noreply.github.com>
2022-08-15 13:47:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by <a href="https://github.com/t-taia/" target="_blank">Taia </a> and is{" "}  
-      <a href="https://github.com/t-taia/react-weather-app" target="_blank">
-       open-sourced on GitHub</a>
+        This project was coded by 
+        <a href="https://github.com/t-taia/" target="_blank">
+          Taia </a> and is{" "}  
+        <a href="https://github.com/t-taia/react-weather-app" target="_blank">
+          open-sourced on GitHub</a>
       </footer>
     </div>
   )
