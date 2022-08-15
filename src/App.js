6969346f20cd79eeeebb3b240c1e3329c5e6d732
t-taia <95856208+t-taia@users.noreply.github.com>
2022-08-15 14:13:27 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
+      
+      {/*---Footer---*/}
       <footer>
-        This project was coded by 
+        This project was coded by {" "}
         <a href="https://github.com/t-taia/" target="_blank">
-          Taia </a> and is{" "}  
+          Taia</a> and is{" "}  
         <a href="https://github.com/t-taia/react-weather-app" target="_blank">
-          open-sourced on GitHub</a>
+        open-sourced on GitHub</a>
       </footer>
+      </div>
     </div>
   )
 }
