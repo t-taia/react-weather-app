7637d9bc42c49cf93d79ca100824bcc67876fff1
t-taia <95856208+t-taia@users.noreply.github.com>
2022-08-15 12:25:09 +0200

    added the github link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://github.com/t-taia/" target="_blank">Taia </a> and is{" "}  
+      <a href="https://github.com/t-taia/react-weather-app" target="_blank">
+       open-sourced on GitHub</a>
+      </footer>
     </div>
   )
 }
