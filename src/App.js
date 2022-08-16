554cbfee6dce598618bb343203c334507f1365a0
t-taia <95856208+t-taia@users.noreply.github.com>
2022-08-16 13:33:32 +0200

    added dependencies, API

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <div className="App">
       <div className="container">
       
-      <Weather />
+      <Weather defaultCity="Paris" />
       
       {/*---Footer---*/}
       <footer>
--- src/Weather.js
+++ src/Weather.js
@@ -1,69 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather () {
-    return (
-    <div className="Weather">
-        {/* Search block*/}
-        <form>
-            <div className="row">
-                <div className="col-9">
-                    <input 
-                    type="search" 
-                    placeholder="Enter a city..." 
-                    className="form-control"
-                    autoFocus="on"
-                    />
-                </div>
 
-                <div className="col-3">
-                    <input 
-                    type="submit" 
-                    value="Search"
-                    className="btn btn-secondary w-100"/>
-                </div>
-            </div>
-        </form>
+ 
 
-        {/* Weather description block*/}
-        <h1>Paris</h1>
-        <ul>
-            <li>Monday 12:00</li>
-            <li>Cloudy</li>
-        </ul>
+export default function Weather (props) {
+    
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    function handleResponse(response) {
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            city: response.data.name,
+            date: "Monday 12:00",
+            temperature: response.data.main.temp,
+            description: response.data.weather[0].description,
+            humidity: response.data.main.humidity,
+            wind: response.data.wind.speed,
+            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+        });
 
-        {/* IMG and Temperature block*/}
-        <div className="row mt-3">
-           
-            <div className="col-6">
-                <div className="clearfix">
-                    <img 
-                    src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" 
-                    alt="Cloudy" 
-                    />
-                    
-                    <span className="temperature">22</span>
-                    <span className="unit">°C</span>
-                </div>
-            </div>
+     
+    }
+
+    if (weatherData.ready) {
+        return (
+            <div className="Weather">
+                {/* Search block*/}
+                <form>
+                    <div className="row">
+                        <div className="col-9">
+                            <input 
+                            type="search" 
+                            placeholder="Enter a city..." 
+                            className="form-control"
+                            autoFocus="on"
+                            />
+                        </div>
         
-
-        {/* Conditions block*/}
-            <div className="col-6">
+                        <div className="col-3">
+                            <input 
+                            type="submit" 
+                            value="Search"
+                            className="btn btn-secondary w-100"/>
+                        </div>
+                    </div>
+                </form>
+        
+                {/* Weather description block*/}
+                <h1>{weatherData.city}</h1>
                 <ul>
-                    <li>
-                        Precipitation: 5%
-                    </li>
-                    <li>
-                        Humidity: 76%
-                    </li>
-                    <li>
-                        Wind: 5 km/h
-                    </li>
+                    <li>{weatherData.date}</li>
+                    <li className="text-capitalize">{weatherData.description}</li>
                 </ul>
+        
+                {/* IMG and Temperature block*/}
+                <div className="row mt-3">
+                   
+                    <div className="col-6">
+                        <div className="clearfix">
+                            <img 
+                            src={weatherData.iconUrl} 
+                            alt={weatherData.description} 
+                            />
+                            
+                            <span className="temperature">{Math.round(weatherData.temperature)}</span>
+                            <span className="unit">°C</span>
+                        </div>
+                    </div>
+                
+        
+                {/* Conditions block*/}
+                    <div className="col-6">
+                        <ul>
+                            <li>
+                                Humidity: {Math.round(weatherData.humidity)}%
+                            </li>
+                            <li>
+                                Wind: {Math.round(weatherData.wind)} km/h
+                            </li>
+                        </ul>
+                    </div>
+        
+                </div>
             </div>
+            );
+    } else {
+        {/* API data*/}
 
-        </div>
-    </div>
-    );
+    const apiKey = "a634dd1dee9be0dfeff6ce65b2525c84";
+   
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+    }
 }
+    
+    
+ 
+    
