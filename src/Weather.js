10866593189d66f68fa83e6be83372225cbdb799
t-taia <95856208+t-taia@users.noreply.github.com>
2022-08-17 10:54:21 +0200

    added weatherIcon comps

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather (props) {
             description: response.data.weather[0].description,
             humidity: response.data.main.humidity,
             wind: response.data.wind.speed,
-            iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+            icon: response.data.weather[0].icon,
         });
     }
 
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
     return (
@@ -18,10 +19,9 @@ export default function WeatherInfo(props) {
            
             <div className="col-6">
                 <div className="clearfix">
-                    <img 
-                    src={props.data.iconUrl} 
-                    alt={props.data.description} 
-                    />
+                   
+                    <WeatherIcon code={props.data.icon} />
+                    
                     
                     <span className="temperature">{Math.round(props.data.temperature)}</span>
                     <span className="unit">°C</span>
